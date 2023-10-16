8013e3bb58bf1f99c34391c00ccf7e79e83f1612
jaydwivedi12 <jayprakashdwivedi12@gmail.com>
2023-10-16 18:47:31 +0530

    middleware modified

--- server/Middleware/auth.js
+++ server/Middleware/auth.js
@@ -34,6 +34,7 @@ const auth = async (req, res, next) => {
         return res.status(401).json({
             success:false,
             message:'Something went wrong while validating the token',
+            data:token
         });
     }
 }
