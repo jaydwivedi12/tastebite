769592e27fdcadc077590e3c162781f9671d0cb1
jaydwivedi12 <jayprakashdwivedi12@gmail.com>
2023-10-16 13:37:30 +0530

    config fixed

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,10 @@
-const SERVER_URI = process.env.NODE_ENV === 'prod' ? 
-'https://tastebite-backend.vercel.app' : 'http://localhost:8080'
+let SERVER_URI;
+const env = process.env.NODE_ENV;
+
+if (env === "development") {
+  SERVER_URI = 'http://localhost:8080';
+} else {
+  SERVER_URI = 'https://tastebite.backend.vercel.app';
+}
 
 export default SERVER_URI;
